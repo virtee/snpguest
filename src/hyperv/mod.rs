992e17432c2f942fa99e678a68e75d5a347b3423
Tyler Fanelli <tfanelli@redhat.com>
2023-07-25 13:09:42 -0400

    hyperv: Check for hypervisor guest + Hyper-V functions
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/hyperv/mod.rs
+++ src/hyperv/mod.rs
@@ -4,6 +4,11 @@ use super::*;
 
 use std::arch::x86_64::__cpuid;
 
+const CPUID_GET_HIGHEST_FUNCTION: u32 = 0x80000000;
+const CPUID_PROCESSOR_INFO_AND_FEATURE_BITS: u32 = 0x1;
+
+const CPUID_FEATURE_HYPERVISOR: u32 = 1 << 31;
+
 const CPUID_HYPERV_SIG: &str = "Microsoft Hv";
 const CPUID_HYPERV_VENDOR_AND_MAX_FUNCTIONS: u32 = 0x40000000;
 const CPUID_HYPERV_FEATURES: u32 = 0x40000003;
@@ -16,6 +21,16 @@ const CPUID_HYPERV_ISOLATION_TYPE_MASK: u32 = 0xf;
 const CPUID_HYPERV_ISOLATION_TYPE_SNP: u32 = 2;
 
 pub fn present() -> bool {
+    let cpuid = unsafe { __cpuid(CPUID_PROCESSOR_INFO_AND_FEATURE_BITS) };
+    if (cpuid.ecx & CPUID_FEATURE_HYPERVISOR) == 0 {
+        return false;
+    }
+
+    let cpuid = unsafe { __cpuid(CPUID_GET_HIGHEST_FUNCTION) };
+    if cpuid.eax < CPUID_HYPERV_VENDOR_AND_MAX_FUNCTIONS {
+        return false;
+    }
+
     let cpuid = unsafe { __cpuid(CPUID_HYPERV_VENDOR_AND_MAX_FUNCTIONS) };
     if cpuid.eax < CPUID_HYPERV_MIN || cpuid.eax > CPUID_HYPERV_MAX {
         return false;
