09ce7b85e315860f332b1f19bb3534c99f49990f
AdithyaKrishnan <AdithyaKrishnan.Kannan@amd.com>
2023-08-10 19:36:03 +0000

    Adding Derived Key Feature
    Signed-off-by: AdithyaKrishnan <AdithyaKrishnan.Kannan@amd.com>

--- src/display.rs
+++ src/display.rs
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use super::*;
-use std::path::PathBuf;
+use std::{path::PathBuf, str};
 
 #[derive(StructOpt)]
 pub enum DisplayCmd {
     #[structopt(about = "Display an attestation report in console.")]
     Report(report_display::Args),
+
+    #[structopt(about = "Display the derived key in console.")]
+    Key(key_display::Args),
 }
 
 pub fn cmd(cmd: DisplayCmd, quiet: bool) -> Result<()> {
     match cmd {
         DisplayCmd::Report(args) => report_display::display_attestation_report(args, quiet),
+        DisplayCmd::Key(args) => key_display::display_derived_key(args, quiet),
     }
 }
 mod report_display {
@@ -30,6 +34,35 @@ mod report_display {
 
         if !quiet {
             println!("{}", att_report);
+        };
+
+        Ok(())
+    }
+}
+
+mod key_display {
+    use super::*;
+
+    #[derive(StructOpt)]
+    pub struct Args {
+        #[structopt(help = "Path to display key.")]
+        pub key_path: PathBuf,
+    }
+
+    // Print derived key in console
+    pub fn display_derived_key(args: Args, quiet: bool) -> Result<()> {
+        let key_report = key::read_key(args.key_path).context("Could not open key")?;
+
+        if !quiet {
+            let mut keydata: String = String::new();
+            for (i, byte) in key_report.iter().enumerate() {
+                if (i % 16) == 0 {
+                    keydata.push('\n');
+                }
+                keydata.push_str(&format!("{byte:02x} "));
+            }
+            keydata.push('\n');
+            println!("{}", keydata);
         };
 
         Ok(())
---
+++ src/key.rs
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use super::*;
+use sev::firmware::guest::{DerivedKey, Firmware, GuestFieldSelect};
+use std::io::Read;
+use std::{fs, path::PathBuf};
+
+#[derive(StructOpt)]
+pub struct KeyArgs {
+    #[structopt(help = "This is the directory where the derived key will be saved")]
+    pub key_path: PathBuf,
+
+    #[structopt(
+        help = "This is the root key from which to derive the key. Input either VCEK or VMRK. VLEK is not supported yet."
+    )]
+    pub rks: String,
+
+    #[structopt(
+        long = "vmpl",
+        short,
+        help = "Specify VMPL level the Guest is running on. Defaults to 1."
+    )]
+    pub vmpl: Option<u32>,
+
+    #[structopt(
+        long = "guest_field_select",
+        short = "gfs",
+        help = "Specify which Guest Field Select bits to enable. Value of N for Guest Policy:0, Image ID:1, Family ID:2, Measurement:3, SVN:4, TCB Version:5 where Guest Field Select bit = 10^N"
+    )]
+    pub gfs: Option<String>,
+
+    #[structopt(
+        long = "guest_svn",
+        short = "gs",
+        help = "Specify the guest SVN to mix into the key. Must not exceed the guest SVN provided at launch in the ID block."
+    )]
+    pub gsvn: Option<u32>,
+
+    #[structopt(
+        long = "tcb_version",
+        short,
+        help = "Specify the TCB version to mix into the derived key. Must not exceed CommittedTcb"
+    )]
+    pub tcbv: Option<u64>,
+}
+
+pub fn get_derived_key(args: KeyArgs) -> Result<()> {
+    let rks = match args.rks.as_str() {
+        "vcek" => false,
+        "vmrk" => true,
+        _ => return Err(anyhow::anyhow!("Invalid input. Enter either vcek or vmrk")),
+    };
+
+    let vmpl = match args.vmpl {
+        Some(level) => {
+            if level <= 3 {
+                level
+            } else {
+                return Err(anyhow::anyhow!("Invalid vmpl."));
+            }
+        }
+        None => 1,
+    };
+
+    let gfs = match args.gfs {
+        Some(gfs) => {
+            let value: u64 = u64::from_str_radix(gfs.as_str(), 2).unwrap();
+            if value <= 63 {
+                value
+            } else {
+                return Err(anyhow::anyhow!("Invalid gfs."));
+            }
+        }
+        None => 0,
+    };
+
+    let gsvn: u32 = args.gsvn.unwrap_or(0);
+
+    let tcbv: u64 = args.tcbv.unwrap_or(0);
+
+    let request = DerivedKey::new(rks, GuestFieldSelect(gfs), vmpl, gsvn, tcbv);
+    let mut sev_fw = Firmware::open().context("failed to open SEV firmware device.")?;
+    let derived_key: [u8; 32] = sev_fw
+        .get_derived_key(None, request)
+        .context("failed to request derived key")?;
+
+    // Create derived key path
+    let key_path: PathBuf = args.key_path;
+
+    // Write derived key into desired file
+    let mut key_file = if key_path.exists() {
+        std::fs::OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open(key_path)
+            .context("Unable to overwrite derived key file contents")?
+    } else {
+        fs::File::create(key_path).context("Unable to create attestation report file contents")?
+    };
+
+    bincode::serialize_into(&mut key_file, &derived_key)
+        .context("Could not serialize attestation report into file.")?;
+
+    Ok(())
+}
+
+pub fn read_key(key_path: PathBuf) -> Result<Vec<u8>, anyhow::Error> {
+    let mut key_file = fs::File::open(key_path)?;
+    let mut key = Vec::new();
+    // read the whole file
+    key_file.read_to_end(&mut key)?;
+    Ok(key)
+}
