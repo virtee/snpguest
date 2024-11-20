5bba91fb7146d39f7f2c2e190e1b8d6732016542
Harika Nittala <lnittala@amd.com>
2024-11-20 20:05:57 +0000

    Re-test snpguest PR CI test workflow for latest commit
    Signed-off-by: Harika Nittala <lnittala@amd.com>

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 // This is the main entry point of the snpguest utility. The CLI includes subcommands for requesting and managing certificates, displaying information, fetching derived keys, and verifying certificates and attestation reports.
 
 //comment added for snptest PR purpose
+//commit 2 for re-test PR test for latest commie
 mod certs;
 mod display;
 mod fetch;
